78e859c9dc189c306b4793c4e8e1f39de33f844f
sakii-11 <149531974+sakii-11@users.noreply.github.com>
2023-11-10 14:32:18 +0530

    Create 	702A-MaximumIncrease.cpp

---
+++ 9-11-23/702A-MaximumIncrease.cpp
@@ -0,0 +1,28 @@
+Question :	702A - Maximum Increase
+
+code:
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int count =0, maxi=0;
+    int a,b=0;
+    for(int i=0; i<n; i++)
+    {
+        cin >> a;
+        if(b<a)
+        {
+            count++;
+            maxi= max(maxi, count);
+        }
+        else
+        count =1;
+        b=a;
+    }
+    cout << maxi << endl;
+}
